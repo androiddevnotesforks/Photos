622d1762ed5448c4625a4fcf9b76b4e684b94341
SIKV <serhii.here@gmail.com>
2026-03-19 15:42:29 +0200

    Bump to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
 
     defaultConfig {
         applicationId = "com.github.sikv.photos"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
